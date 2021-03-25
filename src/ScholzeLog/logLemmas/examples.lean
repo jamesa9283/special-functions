6ad5759e6ec823ea17115f35cae31fca7169cc6a
James Arthur <jamesarthur7@icloud.com>
2021-03-25 18:29:57 +0000

    fixed errors

--- src/ScholzeLog/logLemmas/examples.lean
+++ src/ScholzeLog/logLemmas/examples.lean
@@ -1,5 +1,7 @@
 import .tactics .ScholzeHelpLemmas
 
+open real
+
 example (s : ℝ) (h : 0 < s) : 4*s^s = exp(log 4 + s*log s) :=
 begin
   rw [exp_add, exp_log (show 0 < (4 : ℝ), by norm_num), ← log_rpow h, exp_log],
